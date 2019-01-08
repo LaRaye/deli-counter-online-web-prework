1f3fd743bce93aa784385adb62fc79e7617e28f7
LaRaye Johnson <lnj2105@gmail.com>
2019-01-08 18:12:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ end
 def take_a_number(katz_deli, customer)
   katz_deli.push(customer)
   
-  puts "Welcome, #{customer}. You are number #{katz_deli.index(customer) + 1} in line."
+  puts "Welcome, #{customer}. You are number #{katz_deli.size} in line."
 end
 
 def now_serving(katz_deli)
